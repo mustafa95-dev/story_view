73c652f4df53cadc5ab60e3bb4f1724834479f4e
Mustafa Ihsan <mustafaihsanalidev@gmail.com>
2022-04-15 12:55:26 +0300

    .

--- lib/widgets/story_view.dart
+++ lib/widgets/story_view.dart
@@ -795,7 +795,7 @@ class StoryProgressIndicator extends StatelessWidget {
 
   StoryProgressIndicator(
     this.value, {
-    this.indicatorHeight = 2,
+    this.indicatorHeight = 0.5,
   });
 
   @override
